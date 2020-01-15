dab82775c81c21ece80f0c0d353f09fab14ba097
Romulo <romulo.acf@gmail.com>
2020-01-15 16:42:09 +0000

    Criacao classe posicao

---
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao()
+        {
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha +
+                ", " +
+                Coluna;
+        }
+
+    }
+}
